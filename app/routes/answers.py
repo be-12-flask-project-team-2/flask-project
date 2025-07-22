828554ee07fd7d698c5e24cc191fb2ea57e238fd
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-07-22 10:15:04 +0900

    ✨ Add code
    add code

--- app/routes/answers.py
+++ app/routes/answers.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify, Blueprint, request
+from app.models import Answer
+from app import db
 
-answers_bp = Blueprint("answers", __name__, url_prefix="/answers")
+answers_blp = Blueprint("answers", __name__)
 
-@answers_bp.route("/stats/answer_rate_by_choice", methods=["GET"])
-def rate_by_answer():
-    return jsonify({
-        "question_id": 1,
-        "choice_id": 1,
-        "answer_count": 5,
-        "percentage": 20.0
-    })
+@answers_blp.route("/submit", methods=["POST"])
+def submit_answers():
+    data = request.get_json()
+
+    user_id = data[0]["user_id"]
+    for item in data:
+        answer = Answer(user_id=item["user_id"], choice_id=item["choice_id"])
+        db.session.add(answer)
+
+    db.session.commit()
+    return jsonify({"message": f"User: {user_id}'s answers Success Create"}), 200
--- app/routes/images.py
+++ app/routes/images.py
@@ -1,8 +1,32 @@
 from flask import Flask, jsonify, Blueprint
+from app.models import Image
 
-image_bp = Blueprint("image", __name__, url_prefix="/images")
+images_blp = Blueprint("image", __name__, url_prefix="/images")
 
-@image_bp.route("/main", methods=["GET"])
-def get_image():
-    return jsonify({"image": "https://images.unsplash.com/photo-1614522407266-ad3c5fa6bc24?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"})
+images = []
+image_id_counter = 1
 
+@images_blp.route("/", methods=["POST"])
+def create_image():
+    global image_id_counter
+
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"message": "Invalid request data"}), 400
+
+    image = {
+        "url": data["url"],
+        "type": data["type"]
+    }
+    images.append(image)
+    image_id_counter += 1
+
+    return jsonify({
+        "message": f"ID: {image['id']} Image Success Create"
+    }), 200
+
+@images_blp.route("/main", methods=["GET"])
+def main_image():
+    main_image = Image.query.filter_by(type="main").first()
+    return jsonify({"image": main_image.url})
